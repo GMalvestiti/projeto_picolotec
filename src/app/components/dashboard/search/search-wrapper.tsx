03ddf0ba1234a37bb647cfc52451e3f10a2ffce6
GMalvestiti <gustavo.smalvestiti@gmail.com>
2023-11-06 19:16:36 -0300

    added main content container
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

---
+++ src/app/components/dashboard/search/search-wrapper.tsx
@@ -0,0 +1,20 @@
+import { styled } from "@mui/material/styles";
+import SearchIcon from "@mui/icons-material/Search";
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+export default function SearchWrapper() {
+  return (
+    <SearchIconWrapper>
+      <SearchIcon />
+    </SearchIconWrapper>
+  );
+}
